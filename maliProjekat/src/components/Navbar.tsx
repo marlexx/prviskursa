7781956ba69f486f63c0c28d4d93464dc98b9b25
marlexx <122455888+marlexx@users.noreply.github.com>
2024-01-17 18:19:53 +0100

    initial commit

---
+++ maliProjekat/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+        <img src="../../public/react-logo.png" className="nav--icon"></img>
+        <h1 className="nav--logo_text">Stasa{'<3'}</h1>
+        <h2 className="nav--title">Stasa Course - Project 1</h2>
+        </nav>
+    )
+}
