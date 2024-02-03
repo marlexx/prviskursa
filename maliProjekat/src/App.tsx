a87cc4284092650c00c8c14b417cfaa3e05d696d
marlexx <122455888+marlexx@users.noreply.github.com>
2024-02-03 21:48:33 +0100

    themes

--- maliProjekat/src/App.tsx
+++ maliProjekat/src/App.tsx
@@ -1,13 +1,21 @@
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-
+  const [mode, setMode] = useState('dark');
+  function toggleMode() {
+    setMode((mode) => {
+      console.log('clicked')
+      return (mode === 'dark' ? 'light' : 'dark')
+    })
+  }
+  console.log(mode)
   return (
     <>
-    <Navbar />
-    <Main />
+    <Navbar mode={mode} changeMode={toggleMode}/>
+    <Main mode={mode}/>
     </>
   )
 }
--- maliProjekat/src/components/Navbar.tsx
+++ maliProjekat/src/components/Navbar.tsx
@@ -1,10 +1,23 @@
 
-export default function Navbar() {
+export default function Navbar(props : any) {
     return (
-        <nav className="nav">
+        <nav className={props.mode === 'dark' ? "nav dark" : "nav light"}>
         <img src="/react-logo.png" className="nav--icon"></img>
-        <h1 className="nav--logo_text">Staša{'<3'}</h1>
-        <h2 className="nav--title">Staša Course - Project 1</h2>
+        <h1 className={props.mode === 'dark' ? "nav--logo_text-dark" : "nav--logo_text-light"}>Staša{'<3'}</h1>
+        <h2 className={props.mode === 'dark' ? "nav--title_text-dark" : "nav--logo_title-light"}>Staša Course - Project 1</h2>
+        <div 
+                className="toggler" 
+            >
+                <p className="toggler--dark">Dark</p>
+                
+                <div 
+                    className="toggler--slider"
+                    onClick={props.changeMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--light">Light</p>
+            </div>
         </nav>
     )
 }
